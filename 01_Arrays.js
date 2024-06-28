2b38fa478fa477cdd4dbe14f5ce573de2a5a5a44
Abdullah-MWT <abdullahdeveloper2018@gmail.com>
2024-06-28 23:18:37 +0500

    My Second JS Code Folder

--- 01_Arrays.js
+++ 01_Arrays.js
@@ -1,1 +1,19 @@
-console.log("Hi");
+// ************************  ARRAYS  **********************
+
+let myCars = ["Ferrari", "BMW", "Toyota", "Corrola", "Fortuner", "Tesla"]; // "Used to store multiptle values in a single variable"
+console.log(myCars[3]);
+
+//**************  Different Array Methods **************
+
+// 1:-  .push  "Used to add new values into the array"
+myCars.push("Mercedes"); 
+console.log(myCars);
+
+
+// 2:-  .pop  "Used to remove the last value in an Array"
+myCars.pop();
+console.log(myCars);
+
+
+// 3:-  .unshift and .shift just works like .push and .pop respectively : Means unshift
+
