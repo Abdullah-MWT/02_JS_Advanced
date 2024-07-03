1758fc223345f4a5360c5ad96964c840b24671ab
Abdullah-MWT <abdullahdeveloper2018@gmail.com>
2024-07-03 17:03:52 +0500

    My Second JS Code Folder

--- 02_Arrays.js
+++ 02_Arrays.js
@@ -18,3 +18,65 @@ console.log(concated); // It works opposite of .push , means that it merges all 
 // 3:-   spread-method   
 const spreaded = [...arr1, ...arr2];
 console.log(spreaded);     // it works the same as .concat but  it,s  more preferred
+
+
+
+// Now , what f there are Arrays inside Array. Means Nested arrays : for this purpose we can use .flat method 
+const myArray = [1, 2, 3, 4, [5, 6], 7, [8, 9, [4, 5]]]; 
+const flatArray = myArray.flat(Infinity);
+console.log(flatArray);
+
+
+// while scrapping data from somwhere, we might get difffernt dataTypes but we it in Array. Let,s see this case
+console.log(Array.isArray("Abdullah")); // Asking just, that is it an Array or not
+
+console.log(Array.from("Abdullah"));   // Used for converting other dataType into an Array
+
+
+
+// .map  "Used to perform action on each element of an Array"
+const nums = [2, 4, 8, 16, 32, 64, 128, 256];
+function myFuncton(digit) {
+    return (digit * 2) +1 ;
+}
+
+let squareRoots = nums.map(myFuncton);
+console.log(squareRoots);
+
+
+
+// .filter  "Used to filter out somethings out of an Array"
+function myFunction2(value) {
+    return value < 100 ;
+}
+
+let filteredNums = nums.filter(myFunction2);
+console.log(filteredNums);
+
+
+
+// .reduce  "Used to perform action on each value to combine or reduce them into  a  single value"
+function myFunction3(a,b) {
+    return a + b
+}
+
+let reducedNums =  nums.reduce(myFunction3);
+console.log(reducedNums);
+
+
+//   .every     "Checks each element of an Array to check whether it follows the below instructions or not"
+function myFunction4(value1) {
+    return value1 > 18
+}
+let everyChecked = nums.every(myFunction4);
+console.log(everyChecked);
+
+
+
+//  .some     "Checks Elements of an Array to check whether any element follows the instructuons or not"
+function myFunction5(value2) {
+    return value2 > 18
+}
+let someChecked = nums.some(myFunction5);
+console.log(someChecked);
+
---
+++ 02_Objects.js
@@ -0,0 +1,34 @@
+// ******************   OBJECTS   ********************
+
+//  literals:-                  Note: Key of objects is always treated as String behind the scenes
+
+const jsDevs = {
+    name:  "Abdullah",
+    emial: "abdullah@google.com",
+    "full Name": "Abdullah Developer",
+    age: "18",
+    address: "Lahore",
+    isLoggedIn: false,
+
+}
+// We can access the values of arrays either by name or value itslef through two main ways
+
+
+// way# 1  
+console.log(jsDevs.age);   // "The simple and most common method of accessing but here are some porblems"
+console.log(jsDevs.emial);
+// console.log(jsDevs.full Name);  // it,ll give us error
+
+
+
+// way# 2
+// But what if we want to access the "full Name" here through the way# 1 , it will give us error , that,s why here is way#2
+console.log(jsDevs["full Name"]);  // it,s correct syntax for accessing such type of values
+
+
+
+// changing values in Objects
+ jsDevs.emial = "abdullah@apple.com";
+ Object.freeze(jsDevs);   // it,ll freeze the object and no more values will be changed after that
+ jsDevs.emial = "abdullah@microsoft.com";
+ console.log(jsDevs.emial);  
