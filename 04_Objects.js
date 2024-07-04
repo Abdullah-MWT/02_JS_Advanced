2d42c62aa151a478174721642a14d8dd9ac66651
Abdullah-MWT <abdullahdeveloper2018@gmail.com>
2024-07-04 18:37:51 +0500

    My Second JS Code Folder

--- 04_Objects.js
+++ 04_Objects.js
@@ -19,3 +19,16 @@ const userApp = {
 }
 
 console.log(userApp.userName.userFullName.userFirstName);  // Accessing the firstName inside nested objects
+
+
+
+// Merging Objects with Objesct    2 ways for doing this
+const obj1 = {1: "A",  2: "B", 3: "C"};
+const obj2 = {4: "D",  5: "E", 6: "F"};
+
+const obj3 = Object.assign({}, obj1, obj2);  // Here the {} represents the target while others are sources, Less used and not Recommended 
+console.log(obj3);
+ 
+
+const obj4 = {...obj1, ...obj2};   // Most Used and Recommended
+console.log(obj4);
